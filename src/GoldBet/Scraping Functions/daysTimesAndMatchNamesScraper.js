884ee1f9ba6517cd16b6c2ee0efd80df2dd5c5dd
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-09-03 05:02:27 +0200

    .

--- src/GoldBet/Scraping Functions/daysTimesAndMatchNamesScraper.js
+++ src/GoldBet/Scraping Functions/daysTimesAndMatchNamesScraper.js
@@ -1,18 +1,23 @@
-const daysAndTimesScraper = async (html, By, selector) => {
+const daysTimesAndMatchNamesScraper = async (html, By, selector) => {
   try {
     let days = [];
     let times = [];
+    let homes = [];
+    let aways = [];
 
     let rows = await html.findElements(By.css(selector));
     for (let i = 0; i < rows.length; i++) {
       let info = await rows[i].getAttribute("data-evndate");
+      let matchName = await rows[i].getAttribute("data-evtname");
       days.push(info.split(" ")[0].replace("-", "/").replace("-", "/"));
       times.push(info.split(" ")[1]);
+      homes.push(matchName.split(" - ")[0]);
+      aways.push(matchName.split(" - ")[1]);
     }
-    return { days, times };
+    return { days, times, homes, aways };
   } catch (error) {
     console.log("Nothing found by daysAndTimesScraper function");
   }
 };
 
-module.exports = daysAndTimesScraper;
+module.exports = daysTimesAndMatchNamesScraper;
--- src/GoldBet/goldbetScraper.js
+++ src/GoldBet/goldbetScraper.js
@@ -3,10 +3,8 @@ const { JSDOM } = require("jsdom");
 const { window } = new JSDOM();
 
 // Scraping Functions
-const matchNamesScraper = require("./Scraping Functions/matchNamesScraper");
 const oddsScraper = require("./Scraping Functions/oddsScraper");
-const daysAndTimesScraper = require("./Scraping Functions/daysAndTimesScraper");
-const sportNationTournamentScraper = require("./Scraping Functions/sportNationTournamentScraper");
+const daysTimesAndMatchNamesScraper = require("./Scraping Functions/daysTimesAndMatchNamesScraper");
 
 const goldbetScraper = async (
   chrome,
@@ -39,38 +37,57 @@ const goldbetScraper = async (
 
   for (let i = 0; i < links.length; i++) {
     try {
-      const start = window.performance.now();
       await driver.get(links[i]);
       await sleep(50);
       console.log("Scraping:    ", await driver.getCurrentUrl());
-      
-      // Sport type, nation and tournament
-      const sportNationTournament = await sportNationTournamentScraper(
-        driver,
-        By,
-        "brbs breadcrumbs",
-        "li"
-      );
+
+      // Sport type
+      let sportType = links[i].split("/")[4];
+      sportTypeCapitalized = sportType.charAt(0).toUpperCase();
+      sportType = sportTypeCapitalized + links[i].split("/")[4].slice(1);
+
+      // Nation
+      let nation = links[i].split("/")[5];
+      nationCapitalized = nation.charAt(0).toUpperCase();
+      nation = nationCapitalized + links[i].split("/")[5].slice(1);
+
+      // Tournament
+      let tournament = links[i].split("/")[6];
+      let splitCondition = tournament.split("-");
+      if (splitCondition.length >= 2) {
+        let firstPartCapitalized = splitCondition[0].charAt(0).toUpperCase();
+        let secondPartCapitalized = splitCondition[1].charAt(0).toUpperCase();
+        tournament =
+          firstPartCapitalized +
+          splitCondition[0].slice(1) +
+          " " +
+          secondPartCapitalized +
+          splitCondition[1].slice(1);
+      } else {
+        let capitalize = tournament.charAt(0).toUpperCase();
+        tournament = capitalize + tournament.slice(1);
+      }
+
       // Days and times
-      const daysAndTimes = await daysAndTimesScraper(
+      const daysAndTimes = await daysTimesAndMatchNamesScraper(
         driver,
         By,
         "tr[data-evndate]"
       );
       // Match names
-      const matchNames = await matchNamesScraper(driver, By, "eventClick");
+      //const matchNames = await matchNamesScraper(driver, By, "eventClick");
       // Odds
       const odds = await oddsScraper(driver, By, "odd default ");
 
       for (let i = 0; i < daysAndTimes.days.length; i++) {
         const matchInfo = {
-          sport_type: sportNationTournament.sportType,
-          nation: sportNationTournament.nation,
-          tournament: sportNationTournament.tournament,
+          sport_type: sportType,
+          nation: nation,
+          tournament: tournament,
           start_day: daysAndTimes.days[i],
           start_time: daysAndTimes.times[i],
-          home: matchNames.homes[i],
-          away: matchNames.aways[i],
+          home: daysAndTimes.homes[i],
+          away: daysAndTimes.aways[i],
           one: odds.oneOdds[i],
           x: odds.xOdds[i],
           two: odds.twoOdds[i],
@@ -85,10 +102,9 @@ const goldbetScraper = async (
 
         goldbetOdds.push(matchInfo);
       }
-      const stop = window.performance.now();
-      console.log(`Time Taken to execute = ${(stop - start) / 1000} seconds`);
     } catch (error) {}
   }
+  console.log(goldbetOdds.length);
   const goldbetOddsFile = JSON.stringify(goldbetOdds);
   return goldbetOddsFile;
 };
